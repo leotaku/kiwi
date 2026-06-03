ab23474eaaf7ca3e79be915f80b0c024cd8c98c1
Leo Gaskin <leo.gaskin@le0.gs>
2026-06-03 17:11:38 +0200

    Commit

--- src/typst_routines.rs
+++ src/typst_routines.rs
@@ -1,15 +1,14 @@
-use std::{any::Any, collections::HashMap, ops::Deref};
-
+use rustc_hash::{FxBuildHasher, FxHashMap};
 use typst::{
     Features, Library, LibraryExt as _,
     diag::{SourceDiagnostic, Warned},
-    ecow::{EcoVec, eco_vec},
+    ecow::{EcoString, EcoVec, eco_format, eco_vec},
     engine::Engine,
     foundations::{
         Arg, Args, Content, Dynamic, Element, IntoValue, NativeElement, NativeFunc as _, Packed,
         Recipe, Repr, Selector, Style, Transformation, Value,
     },
-    introspection::{Introspector as _, QueryIntrospection},
+    introspection::{Introspector as _, Location, QueryIntrospection},
     model::RefElem,
     syntax::{Span, Spanned, VirtualPath},
     text::TextElem,
@@ -19,30 +18,43 @@ use typst_html::{HtmlAttr, HtmlElem, HtmlTag};
 
 use crate::typst_world::{GlobalContext, TemporaryWorld};
 
+#[derive(Clone, Debug, PartialEq)]
+struct Page {
+    anchors: FxHashMap<Location, EcoString>,
+    document: ManuallyHash<typst_html::HtmlDocument>,
+}
+
+#[typst_macros::elem]
+pub struct ExtRefElem {}
+
 #[typst_macros::ty]
-#[derive(Hash, Clone, Debug, PartialEq)]
-struct Wiki(Vec<ManuallyHash<typst_html::HtmlDocument>>);
+#[derive(Clone, Debug, PartialEq)]
+pub struct Wiki(FxHashMap<VirtualPath, Page>);
+
+impl std::hash::Hash for Wiki {
+    fn hash<H: std::hash::Hasher>(&self, state: &mut H) {
+        for (path, page) in self.0.iter() {
+            path.hash(state);
+            page.document.root().hash(state);
+        }
+    }
+}
 
 impl Wiki {
     fn empty() -> Self {
-        Self(Vec::new())
+        Self(FxHashMap::with_hasher(FxBuildHasher))
     }
 
-    fn new(documents: Vec<typst_html::HtmlDocument>) -> Self {
-        let hashed = documents
-            .into_iter()
-            .map(|doc| {
-                let hash = hash128(doc.root());
-                ManuallyHash::new(doc, hash)
-            })
-            .collect();
-        Self(hashed)
-    }
-
-    fn query(&self, selector: &Selector) -> EcoVec<Content> {
+    fn query(&self, selector: &Selector) -> EcoVec<(VirtualPath, Content)> {
         let mut results = EcoVec::new();
-        for document in self.0.iter() {
-            results.extend(document.introspector().query(selector));
+        for (path, page) in self.0.iter() {
+            results.extend(
+                page.document
+                    .introspector()
+                    .query(selector)
+                    .into_iter()
+                    .map(|content| (path.clone(), content)),
+            )
         }
 
         results
@@ -72,31 +84,46 @@ fn resolve_refs_externally(
         Ok(packed) => packed,
         Err(content) => return Ok(content.into_value()),
     };
-    let intra_doc_labels = engine.introspect(QueryIntrospection(
+    let intra_doc_labeled = engine.introspect(QueryIntrospection(
         Selector::Label(packed.target),
         Span::detached(),
     ));
-    if intra_doc_labels.len() > 0 {
+    if intra_doc_labeled.len() > 0 {
         return Ok(packed.pack().into_value());
     }
 
-    let inter_doc_labels = wiki.query(&Selector::Label(packed.target));
-    match inter_doc_labels.len() {
-        0 => Err(eco_vec![SourceDiagnostic::error(packed.span(), ":(")]),
-        1 => Ok(HtmlElem::new(HtmlTag::constant("a"))
-            .with_attr(HtmlAttr::constant("href"), "penis")
-            .with_body(Some(TextElem::packed("penis")))
-            .pack()
-            .into_value()),
-        _ => Err(eco_vec![SourceDiagnostic::error(packed.span(), ":(")]),
-    }
+    // TODO: ExtRefElem
+
+    let mut inter_doc_labeled = wiki.query(&Selector::Label(packed.target));
+    let (target_path, target_content) = match inter_doc_labeled.pop() {
+        None => {
+            return Err(eco_vec![SourceDiagnostic::error(packed.span(), ":(")]);
+        }
+        Some(_) if inter_doc_labeled.len() > 0 => {
+            return Err(eco_vec![SourceDiagnostic::error(packed.span(), ":(")]);
+        }
+        Some(queried) => queried,
+    };
+    let target_page = &wiki.0[&target_path];
+
+    let target_link = target_content
+        .location()
+        .and_then(|loc| target_page.anchors.get(&loc))
+        .map(|id| eco_format!("{}#{}", target_path.get_with_slash(), id))
+        .unwrap_or_else(|| target_path.into_with_slash());
+
+    Ok(HtmlElem::new(HtmlTag::constant("a"))
+        .with_attr(HtmlAttr::constant("href"), target_link)
+        .with_body(Some(TextElem::packed("TODO")))
+        .pack()
+        .into_value())
 }
 
 pub fn collect_labels(
     context: &GlobalContext,
     paths: impl IntoIterator<Item = VirtualPath>,
-) -> (Vec<Content>, EcoVec<SourceDiagnostic>) {
-    let mut content = Vec::new();
+) -> (Wiki, EcoVec<SourceDiagnostic>) {
+    let mut pages = FxHashMap::with_hasher(FxBuildHasher);
     let mut errors = EcoVec::new();
 
     let mut library = Library::builder().with_features(Features::all()).build();
@@ -117,23 +144,40 @@ pub fn collect_labels(
         // TODO: generate IDs with AnchorGenerator
 
         match typst::compile::<typst_html::HtmlDocument>(&world).output {
-            Ok(document) => content.extend(document.introspector().query_labelled().into_iter()),
+            Ok(mut document) => {
+                let targets = document
+                    .introspector()
+                    .query_labelled()
+                    .into_iter()
+                    .filter_map(|content| content.location())
+                    .collect();
+                let anchors = typst_html::create_link_anchors(&mut document, &targets);
+
+                let hash = hash128(document.root());
+                pages.insert(
+                    path.with_extension("html"),
+                    Page {
+                        anchors,
+                        document: ManuallyHash::new(document, hash),
+                    },
+                );
+            }
             Err(errs) => errors.extend(errs),
         };
     }
 
-    (content, errors)
+    (Wiki(pages), errors)
 }
 
 pub fn render_wiki(
-    labeled_content: Vec<Content>,
+    wiki: Wiki,
     context: &GlobalContext,
     paths: impl IntoIterator<Item = VirtualPath>,
 ) -> (
-    HashMap<VirtualPath, typst_html::HtmlDocument>,
+    FxHashMap<VirtualPath, typst_html::HtmlDocument>,
     EcoVec<SourceDiagnostic>,
 ) {
-    let mut output = HashMap::new();
+    let mut output = FxHashMap::with_hasher(FxBuildHasher);
     let mut diagnostics = EcoVec::new();
 
     let mut library = Library::builder().with_features(Features::all()).build();
@@ -144,7 +188,7 @@ pub fn render_wiki(
             items: eco_vec![Arg {
                 span: Span::detached(),
                 name: Some("wiki".into()),
-                value: Spanned::detached(Value::Dyn(Dynamic::new(Wiki::empty()))),
+                value: Spanned::detached(Value::Dyn(Dynamic::new(wiki))),
             }],
         })),
         Span::detached(),
